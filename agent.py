5f9bfc5530634cf1f14af772f35b165324746bc7
KentonMewling <tjwright1228@gmail.com>
2026-05-09 15:10:07 -0700

    added a checkpointer to the orch model, it creates an sqlite db and saves context from prior chats to it so model can remember certain things

--- agent.py
+++ agent.py
@@ -1,26 +1,48 @@
 from langchain_ollama import ChatOllama
 from config import orchestrator, temperature
 from langchain.agents import create_agent
-from tools.langchain_tools import get_stats
+from tools.langchain_tools import get_stats, collect_all, classify_all
+from langgraph.checkpoint.sqlite import SqliteSaver
+import logging
+import logging.config
+import json
+import os
+import uuid
+import sqlite3
+
+with open("loggingConfigs/config.json") as f:
+    logging.config.dictConfig(json.load(f))
+
 
 llm = ChatOllama(
-    model= orchestrator,
-    temperature= temperature,
-)
+    model= orchestrator, 
+    temperature= temperature
+    )
+
+# checkpointer for our orch model to understand prior context
+os.makedirs("db", exist_ok=True)
+checkpoint_conn = sqlite3.connect("db/checkpoints.db", check_same_thread=False)
+checkpointer = SqliteSaver(checkpoint_conn)
+
+
 agent = create_agent(
     model=llm,
     tools= [get_stats],
-    system_prompt="You are a helpful assistant for analyzing and classifying GitHub bug reports.  You can query a database of classified bug reports from open source projects.",
-    
+    system_prompt="You are a helpful assistant for analyzing and classifying GitHub bug reports.  You can query a database of classified bug reports from open source projects, you can also collect bug reports from a github repository and classify bug reports",
+    checkpointer= checkpointer,
 ) 
-print("InEx Bug Agent - type 'q' to quit")
+thread_id = uuid.uuid4()
+config = {"configureable": {"thread_id": thread_id}}
+
+
+print(f"InEx Bug Agent - Thread {thread_id[:8]} - type 'q' to quit")
 
 while True: 
     q = input("You: ")
     if q=="q":
         break
     inputs = {"messages": [{"role": "user", "content": q}]}
-    results = agent.invoke(inputs)
+    results = agent.invoke(inputs, config=config)
     
     print(f"Agent: {results['messages'][-1].content}")
     
